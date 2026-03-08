49e6370a0c9868e4de9901423d026d854b7b296b
chmcs2026sohamshelke-rgb <chmcs2026.soham.shelke@gmail.com>
2026-03-08 14:32:51 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,5 +19,5 @@ app.use('/api/transactions', transactionRoutes);
 
 connectDB();
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
