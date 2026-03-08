2732199fa1aee3a14f79a21e7ccd245da55a053e
chmcs2026sohamshelke-rgb <chmcs2026.soham.shelke@gmail.com>
2026-03-08 14:26:29 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({
-    baseURL: '/api',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-});
+const api=axios.create({
+    baseURL:'https://bank-account-management-2.onrender.com'
+})
 
 export default api;
